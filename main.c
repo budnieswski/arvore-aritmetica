f12669d0934e4a7e0c22fdf800bdf5b8e66fd34b
Guilherme Budnieswski <budnieswski@gmail.com>
2016-06-27 17:41:31 -0300

    Leitura de expressao

--- main.c
+++ main.c
@@ -8,5 +8,15 @@ int main(int argc, char **argv)
   Arvore *arvore;
   inicializa(&arvore);
 
+  char expressao[100];
+  fgets(expressao, sizeof(expressao), stdin);
+
+  int i;
+  for (i = 0; (expressao[i] != '\n'); ++i)
+  {
+    printf("%c > %d\n", expressao[i], expressao[i]);
+  }
+  
+
   return 0;
 }
